34a2fd9badf61a09688fb07c4de29503a1939913
Arun PP <zc-arunpp@ZC-Arun-PPs-MacBook-Pro.local>
2021-08-12 14:23:25 +0530

    first

---
+++ HTML/js/plugins.js
@@ -0,0 +1,12 @@
+/**
+ * WOW.js Reveal CSS animation as you scroll down a page
+ **/
+wow = new WOW(
+  {
+	animateClass: 'animated',
+	offset:       100,
+	mobile:       false,       // trigger animations on mobile devices (default is true)
+	
+  }
+);
+wow.init();
